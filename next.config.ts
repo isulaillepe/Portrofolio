4a5fc4201719190cf0a215989ed668a034bc8fea
Isula Rajinda Illeperuma <isulaillepe2024@gmail.com>
2026-06-03 10:16:52 +0530

    refactor: remove static export and basePath configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/Portrofolio',
+  // Safe to leave images unoptimized if you are using external images, 
+  // but you can remove it if you want Vercel's automatic image optimization!
   images: {
     unoptimized: true,
   },
